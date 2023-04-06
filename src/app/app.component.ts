31975cd41e6cab8d3056701b743a4e313c3da7f3
rodrigobrick <rodrigobrick25@gmail.com>
2023-04-06 14:04:35 -0400

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'test';
+export class AppComponent implements OnInit {
+  title = 'CodeSandbox';
+  productos: any;
+  form: FormGroup;
+  productoAgregado: any;
+  productosArr = [];
+  constructor(private http: HttpClient, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nombre: [undefined, Validators.required],
+      precio: [undefined, Validators.required],
+      stock: [undefined, Validators.required],
+    });
+  }
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  async getData() {
+    this.http.get<any>(`${environment.apiUrl}`).subscribe((res) => {
+      this.productos = res.products;
+    });
+  }
+
+  agregarProducto() {
+    this.productoAgregado = {
+      title: this.form.controls['nombre'].value,
+      price: this.form.controls['precio'].value,
+      stock: this.form.controls['stock'].value,
+    };
+    this.productos.unshift(this.productoAgregado);
+  }
+
+  campoValido(campo: string) {
+    return (
+      this.form.controls?.[campo]?.errors &&
+      this.form.controls?.[campo]?.touched
+    );
+  }
 }
